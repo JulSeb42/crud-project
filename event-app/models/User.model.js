f6997896afe4f1935a74eea3826dd100de01791d
Julien Sebag <julien.sebag@me.com>
2021-11-20 16:14:33 +0100

    Added front end password validation + form signup + user model

--- event-app/models/User.model.js
+++ event-app/models/User.model.js
@@ -3,11 +3,15 @@ const { Schema, model } = require("mongoose")
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
     {
-        username: {
+        fullName: {
             type: String,
-            // unique: true -> Ideally, should be unique, but its up to you
+            unique: true,
         },
+        email: String,
         password: String,
+        imgPath: String,
+        imgName: String,
+        publicId: String,
     },
     {
         // this second object adds extra properties: `createdAt` and `updatedAt`
--- event-app/public/js/script.js
+++ event-app/public/js/script.js
@@ -1,7 +1,28 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("event-app JS imported successfully!");
-  },
-  false
-);
+// Password reveal
+const passwordInput = document.querySelector("#password")
+const buttonReveal = document.querySelector(".reveal")
+
+buttonReveal.addEventListener("click", () => {
+    if (passwordInput.getAttribute("type") === "password") {
+        passwordInput.setAttribute("type", "text")
+    } else {
+        passwordInput.setAttribute("type", "password")
+    }
+})
+
+// Password validation
+const textHint = document.querySelector(".password-hint")
+
+textHint.classList.add("hidden")
+
+passwordInput.addEventListener("keyup", () => {
+    textHint.classList.remove("hidden")
+
+    const min = 6
+
+    if (passwordInput.value.length < min) {
+        textHint.innerText = `Only ${min - passwordInput.value.length} characters left`
+    } else {
+        textHint.innerText = "Password is good"
+    }
+})
