e4d95b648bff92072579b04da8d37d0d37de6c56
Julien Sebag <julien.sebag@me.com>
2021-11-22 09:36:31 +0100

    Added event list page

--- event-app/routes/events.js
+++ event-app/routes/events.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
+const Events = require("../models/Event.model")
 
 const loginCheck = () => {
     return (req, res, next) => {
@@ -8,7 +9,27 @@ const loginCheck = () => {
 
 router.get("/", loginCheck(), (req, res, next) => {
     const loggedInUser = req.session.user
-    res.render("events", { user: loggedInUser, doctitle: "Events" })
+
+    Events.find().then(eventFromDb => {
+        res.render("events", {
+            user: loggedInUser,
+            event: eventFromDb,
+            doctitle: "Events",
+        })
+    })
+})
+
+router.get("/events/:id", loginCheck(), (req, res, next) => {
+    const id = req.params.id
+    const loggedInUser = req.session.user
+
+    Events.findById(id).then(eventFromDb => {
+        res.render("events/detail", {
+            doctitle: eventFromDb.title,
+            event: eventFromDb,
+            user: loggedInUser,
+        }).catch(err => next(err))
+    })
 })
 
 module.exports = router
